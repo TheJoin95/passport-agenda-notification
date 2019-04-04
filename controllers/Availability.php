bd59a0ec66fd02e740378fa416cadd25a51d617f
thejoin95 <miki.lombi@gmail.com>
2019-04-04 21:36:26 +0200

    aggiunta regexp per recupero disponibilità

--- controllers/Availability.php
+++ controllers/Availability.php
@@ -6,6 +6,10 @@ Class Availability
 	private $endpoint = ENDPOINT['availability_list'];
 	private $port = 443;
 
+	// GestioneDisponibilitaAction.do?codop=getDisponibilitaCittadino&amp;previous=false&amp;data=05-04-2019
+	private $pagination_string = "/GestioneDisponibilitaAction.do?codop=getDisponibilitaCittadino&previous=@PREVIOUS@&data=@DATA@";
+	private $regexp_list = '/headers="(\w+)">|href="([A-Za-z.?=&;0-9-]+)"|>([\sA-Za-z]+)<\/a>|([\w+,\s\/\d-]+)<|>(\d+)<\/td>/';
+
 	public function __construct(String $endpoint, String $port)
 	{
 		$this->setEndpoint($endpoint);
@@ -29,6 +33,24 @@ Class Availability
 
 		if(preg_match('/\d+\/\d+\/\d+/', $response, $match) === 1)
 			$availability_date = $match[0];
+
+		if(!empty($availability_list))
+		{
+			// 0 => field, 1 => link disponibilità, 2 => sede name, 3 => address, 4 => n. civico
+			if(preg_match_all($this->regexp_list, str_replace(array("\n","\r", "\t"), '', $response), $matches))
+			{
+				for($i = 0; $i < count($matches); $i++)
+				{
+					$availabilities[] = array(
+						'link' => $matches[$i][1],
+						'office' => array(
+							'name' => $matches[$i][2],
+							'address' => $matches[$i][3] . ' ' . $matches[$i][4]
+						)
+					);
+				}
+			}
+		}
 
 		// recuperare le strutture con link relativo e tot disponibilità
 
